9fbfdffcc940eb9d2f830f6aeb626dd6ffc715d0
Maxx <Maxx@DESKTOP-I6P5OQE>
2025-09-24 19:58:27 -0300

    agregando pantallas Insuoms de la orden

--- DTO/ObjetosDTO/CrearInsumoDTO.cs
+++ DTO/ObjetosDTO/CrearInsumoDTO.cs
@@ -5,5 +5,9 @@
         public int Id { get; set; }
         public string Nombre { get; set; }
         public int CantDisponible { get; set; }
+
+        //prueba para el textbox de agregarInsumoOrden
+        public int? CantidadARetirar { get; set; }
+
     }
 }
